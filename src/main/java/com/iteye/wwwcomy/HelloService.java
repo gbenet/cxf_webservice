a0dec940254b1065c1b79697389fb0e20dfa8b7c
zhangbin <gbenet@yeah.net>
2019-05-09 10:31:50 +0800

    first

--- src/main/java/com/iteye/wwwcomy/HelloService.java
+++ src/main/java/com/iteye/wwwcomy/HelloService.java
@@ -2,14 +2,20 @@ package com.iteye.wwwcomy;
 
 import javax.jws.WebService;
 
+import com.sun.javafx.tools.packager.Log;
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Component;
 
 @Component
 @WebService(serviceName = "helloService", endpointInterface = "com.iteye.wwwcomy.IHelloService")
 public class HelloService implements IHelloService {
 
+
+    private static Logger log = Logger.getLogger(HelloService.class.getClass());
+
 	public String sayHello(String username) {
-		return username + "! Welcome to CXF in Method[HelloService sayHello]";
+		log.info(username);
+		return "欢迎调用本helloService接口";
 	}
 
 }
